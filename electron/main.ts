033575647a1418b05088cb13927b2da1bae9993e
iboffa <i.boffa@hotmail.it>
2022-12-29 18:21:00 +0100

    Initial commit

---
+++ electron/main.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+
+import { app, BrowserWindow } from 'electron';
+
+function createWindow() {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  // Load the index.html of the app.
+  win.loadFile(path.join(__dirname, '../app/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
