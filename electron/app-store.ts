ab1626aef097e1f6366c863e00ab08e2e82120cd
iboffa <i.boffa@hotmail.it>
2023-01-11 16:37:21 +0200

    implemented electron store

---
+++ electron/app-store.ts
@@ -0,0 +1,31 @@
+import { ipcMain } from 'electron';
+import * as Store from 'electron-store';
+
+export class AppStore {
+
+    private static _store: Store;
+    private constructor() { };
+
+    static init(options?: Store.Options<any>) {
+        if (!this._store) {
+            this._store = new Store(options);
+            ipcMain.on('store:set', (event, prop: string, value: any) => this._store.set(prop, value));
+            ipcMain.on('store:get', (event, prop: string) => { event.returnValue = this._store.get(prop) });
+            ipcMain.on('store:delete', (event, prop: string) => { this._store.delete(prop)});
+        }
+    }
+
+    // Since _store is private, these methods are needed to access the store within the Electron Process
+
+    static set(prop: string, value: any) {
+        this._store.set(prop, value)
+    }
+
+    static get(prop: string) {
+        return this._store.get(prop)
+    }
+
+    static delete(prop: string) {
+        this._store.delete(prop);
+    }
+}
